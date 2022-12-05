a0171e20b2572f87e05e6fbe8ae6b6a0e2519605
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-12-05 01:25:48 -0500

    working rust guessing number game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Lets guess a number!");
+    println!("Lets guess a number from 1-100!");
 
     println!("Please input your guess.");
 
@@ -15,4 +17,15 @@ fn main() {
     
         print!("You guessed: {guess}");
 
+    let guess: u32 = guess.trim().parse().expect("Please only enter numbers");
+
+    let mut rng = thread_rng();
+    let answer = rng.gen_range(0..100);
+    
+    // Exhaustive handling of conditional statements
+    match guess.cmp(&answer) {
+        Ordering::Less => print!("Too low, nice try"),
+        Ordering::Greater => print!("Too high, nice try"),
+        Ordering::Equal => print!("** CORRECT **"),
+    }
 }
