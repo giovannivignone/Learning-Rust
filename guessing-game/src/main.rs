a5b40c23e43d1f918721c0f8d21fab0be0780f82
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-12-05 00:24:42 -0500

    Adding gitignores and finishing guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Lets guess a number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+        print!("You guessed: {guess}");
+
 }
