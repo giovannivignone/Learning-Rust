c37b6164a965f24a0749d536bc15a772c0679dd6
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-12-04 20:46:46 -0500

    hello world and build

---
+++ first-rust-project/target/debug/deps/first_rust_project-0bb8115bbcc11cf5.d
@@ -0,0 +1,5 @@
+/Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/target/debug/deps/first_rust_project-0bb8115bbcc11cf5: src/main.rs
+
+/Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/target/debug/deps/first_rust_project-0bb8115bbcc11cf5.d: src/main.rs
+
+src/main.rs:
---
+++ first-rust-project/target/debug/first-rust-project.d
@@ -0,0 +1,1 @@
+/Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/target/debug/first-rust-project: /Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/src/main.rs
