83d9baad32ac3f055b83a8c96dd573c7e9fde538
Giovanni Vignone <72773059+giovannivignone@users.noreply.github.com>
2022-12-05 01:49:20 -0500

    Delete first-rust-project/target directory

--- first-rust-project/target/debug/deps/first_rust_project-038bbb8389ac4dac.d
+++
@@ -1,5 +0,0 @@
-/Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/target/debug/deps/first_rust_project-038bbb8389ac4dac.rmeta: src/main.rs
-
-/Users/giovannivignone/VSCodeProjects/rust-intro/first-rust-project/target/debug/deps/first_rust_project-038bbb8389ac4dac.d: src/main.rs
-
-src/main.rs:
